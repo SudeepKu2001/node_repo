9da3d7392448657a409089d4aa5bf2bbfd18b4ad
SudeepKu2001 <sudeepkumarparamguru53@gmail.com>
2024-10-08 12:39:32 +0530

    6th commit

--- math.js
+++ math.js
@@ -56,3 +56,34 @@ switch (char){
         console.log("It's not a vowel");
         break; 
 }
+
+//Task-4:
+//print week of the day in switch statement 
+
+let day=6;
+switch(day){
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    default:
+           
+
+}
